0d5a82fd1aad590d33f13ca78bcdfc548a939b02
Oleg Brezgalov <oleg.brezgalov@gmail.com>
2018-12-29 10:00:08 +0300

    remove unnecessary trait

--- src/Module.php
+++ src/Module.php
@@ -1,14 +1,14 @@
 <?php
 
-namespace app\modules\base;
+namespace Yii2Modules;
 
-use app\modules\traits\RequireModulesTrait;
 use yii\base\BootstrapInterface;
 
 class Module extends \yii\base\Module implements BootstrapInterface
 {
-    use RequireModulesTrait;
-
+    /**
+     * @var array - массив модулей-зависимостей
+     */
     public $requiredModules = [];
 
     /**
@@ -26,6 +26,20 @@ class Module extends \yii\base\Module implements BootstrapInterface
         $path = \Yii::getAlias("@app/modules/{$this->id}/config/urlManager.php");
         if (is_file($path)) {
             $app->getUrlManager()->addRules(require($path), false);
+        }
+    }
+
+    /**
+     * @param \yii\base\Application $app
+     * @param array $names
+     */
+    public function requireModules(Application $app, array $names)
+    {
+        $idString = (@$this->id)? "модуля {$this->id} " : '';
+        foreach ($names as $name) {
+            if (!array_key_exists($name, $app->modules)) {
+                throw new ServerErrorHttpException("Модуль $name указан в требованиях " . $idString . "но не обнаружен в приложении!");
+            }
         }
     }
 }
