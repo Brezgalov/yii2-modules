d1e94126d45a01149c4a7244a1cb08314f6fd86d
Oleg Brezgalov <oleg.brezgalov@gmail.com>
2020-07-06 17:13:21 +0300

    fix db fetch in case when no controller in use. As for testing for example

--- src/GetModuleDbTrait.php
+++ src/GetModuleDbTrait.php
@@ -6,15 +6,24 @@ use yii\db\Connection;
 
 trait GetModuleDbTrait
 {
+    public static function getDefaultDbComponentName()
+    {
+        return 'db';
+    }
+
     /**
      * @param string $dbComponentName
      * @return Connection|null
      */
     public static function getModuleDb($dbComponentName = 'dbConnection')
     {
-        $module = \Yii::$app->controller->module;
+        $module = @\Yii::$app->controller->module;
 
-        $componentName = isset($module->{$dbComponentName}) ? $module->{$dbComponentName} : 'db';
+        $componentName = $module && isset($module->{$dbComponentName}) ? (
+            $module->{$dbComponentName}
+        ): (
+            static::getDefaultDbComponentName()
+        );
 
         return @\Yii::$app->{$componentName};
     }
